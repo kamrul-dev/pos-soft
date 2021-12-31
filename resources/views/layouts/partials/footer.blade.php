2dfbf3220574b7d18292130b6acd033773233c5d
Kamrul Hasan <you@example.com>
2021-12-31 18:06:10 +0600

    Poin of Sale Software version 1.0 first release

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 1.0
+    </div>
+    <strong>Copyright &copy; 2022 <a href="https://ebably.com/">ebably.com</a>.</strong>
+  </footer>
