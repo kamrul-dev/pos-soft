02dec8c9d069a342fbb0e7f3587c3c5bfdf63f1a
Kamrul Hasan <you@example.com>
2022-01-03 23:22:31 +0600

    design color changed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
         <div class="row">
           <div class="col-lg-3 col-6">
             <!-- small box -->
-            <div class="small-box bg-info">
+            <div class="small-box bg-danger">
               <div class="inner">
                   <h3>{{$orders_count}}</h3>
                 <p>Orders Count</p>
@@ -21,7 +21,7 @@
           <!-- ./col -->
           <div class="col-lg-3 col-6">
             <!-- small box -->
-            <div class="small-box bg-success">
+            <div class="small-box bg-warning">
               <div class="inner">
                   <h3>{{config('settings.currency_symbol')}} {{number_format($income, 2)}}</h3>
                 <p>Income</p>
@@ -35,7 +35,7 @@
           <!-- ./col -->
           <div class="col-lg-3 col-6">
             <!-- small box -->
-            <div class="small-box bg-danger">
+            <div class="small-box bg-success">
               <div class="inner">
                 <h3>{{config('settings.currency_symbol')}} {{number_format($income_today, 2)}}</h3>
 
@@ -50,7 +50,7 @@
           <!-- ./col -->
           <div class="col-lg-3 col-6">
             <!-- small box -->
-            <div class="small-box bg-warning">
+            <div class="small-box bg-dark">
               <div class="inner">
                 <h3>{{$customers_count}}</h3>
 
